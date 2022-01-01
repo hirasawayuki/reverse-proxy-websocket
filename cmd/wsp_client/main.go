8a526e2a98298757549c529846f60ac08c0a0b0e
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-01-01 17:59:07 +0900

    feature wsp client cmd

---
+++ cmd/wsp_client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/root-gg/wsp/client"
+)
+
+func main() {
+	ctx := context.Background()
+
+	configFile := flag.String("config", "wsp_client.cfg", "config file path")
+	flag.Parse()
+
+	config, err := client.LoadConfiguration(*configFile)
+	if err != nil {
+		log.Fatalf("Unable to load configuration : %s", err)
+	}
+
+	proxy := client.NewClient(config)
+	proxy.Start(ctx)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	<-sigCh
+
+	proxy.Shutdown()
+}
