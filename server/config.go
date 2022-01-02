9d0e899128eb77abf5d51d6e265f544ff66c40ea
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-01-02 14:41:39 +0900

    feature server config

---
+++ server/config.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Host        string
+	Port        int
+	Timeout     int
+	IdleTimeout int
+	SecretKey   string
+}
+
+func (c Config) GetTimeout() time.Duration {
+	return time.Duration(c.Timeout) * time.Millisecond
+}
+
+func NewConfig() (config *Config) {
+	config = new(Config)
+	config.Host = "127.0.0.1"
+	config.Port = 8080
+	config.Timeout = 1000
+	config.IdleTimeout = 60000
+	return
+}
+
+func LoadConfiguration(path string) (config *Config, err error) {
+	config = NewConfig()
+
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	err = yaml.Unmarshal(bytes, config)
+	if err != nil {
+		return
+	}
+
+	return
+}
